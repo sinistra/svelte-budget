47c457196ebdf44fac7d27d98559e2f52d05aa27
Paul Taylor <paul@nms.com.au>
2020-02-24 08:32:56 +1100

    add expense form

--- src/App.svelte
+++ src/App.svelte
@@ -8,10 +8,15 @@
   // components
   import Navbar from "./Navbar.svelte";
   import ExpenseList from "./ExpenseList.svelte";
+  import Totals from "./Totals.svelte";
+  import ExpenseForm from "./ExpenseForm.svelte";
   // data
   import expenseData from "./expenses";
   // variables
   let expenses = [...expenseData];
+  $: total = expenses.reduce((acc, curr) => {
+    return (acc += curr.amount);
+  }, 0);
   // functions
   function removeExpense(id) {
     expenses = expenses.filter(item => item.id !== id);
@@ -30,8 +35,13 @@
 
 <Navbar />
 <main class="content">
+  <ExpenseForm />
+  <Totals title="total expenses" {total} />
   <ExpenseList {expenses} />
-  <button type="button" class="btn btn-primary btn-block" on:click={clearExpenses}>
+  <button
+    type="button"
+    class="btn btn-primary btn-block"
+    on:click={clearExpenses}>
     clear expenses
   </button>
 </main>
---
+++ src/ExpenseForm.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Title from "./Title.svelte";
+  let name = "";
+  let amount = null;
+</script>
+
+<section>
+  <Title title="add expense" />
+  <form class="expense-form">
+    <div class="form-control">
+      <label for="name">name</label>
+      <input type="text" id="name" bind:value={name}/>
+    </div>
+    <div class="form-control">
+      <label for="amount">amount</label>
+      <input type="number" id="amount" bind:value={amount}/>
+    </div>
+    <p class="form-empty">please fill out all form fields</p>
+    <button type="submit" class="btn btn-block">add expense</button>
+    <button type="button" class="close-btn">
+      <i class="fas fa-times" />
+      close
+    </button>
+  </form>
+</section>
